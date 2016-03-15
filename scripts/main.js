d7827a53a5a25949d621511ba1c2868d247d4c20
anna hunter <annfmonty@gmail.com>
2016-03-14 19:42:04 -0500

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+//Sum and Average//
+var sum = 0; //sets the initial value of sum to a number
+		
+for(var i = 0; i < items.length; i++){  // creates loop through the array for the length of the array
+		var sum = sum + (items[i].price); //get the price of each item and adds them together
+	  	//console.log(sum); //prints sum to the console for checking.
+};
+
+var avg = sum/items.length; //takes total of all items and divides by number of items to get average.
+console.log("The average price is $" + avg.toFixed(2)); //prints average to the console with only two decimal places.
+
+
+//Prices between 14 and 18//
+
+
+var chkPrice = function(){
+	for (var i = 0; i < array.length; i++);
+		if ((items[i].price > 14)&(items[i].price < 18)){
+
+
+		}
+	}
+}
+
+//Find GBP code; print name and price//
+
+
+//Find items made of wood//
+
+
+
+
+
+
+
